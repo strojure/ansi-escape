f4ba579074b44f2b686a8216f8599b506466ac1d
Sergey Trofimov <sergey@trofimov.name>
2023-03-07 17:09:23 +0100

    (chore) Change license to MIT

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject com.github.strojure/ansi-escape "1.1.1-SNAPSHOT"
   :description "ANSI color escape sequences."
   :url "https://github.com/strojure/ansi-escape"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]]
+  :dependencies []
 
-  :profiles {:dev {:dependencies []}})
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.11.1"]]}
+             :dev {:dependencies []}})
