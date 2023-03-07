2f0acfc9657c5d454eda93290921cc8a60ddc5dd
Sergey Trofimov <sergey@trofimov.name>
2023-03-07 17:16:59 +0100

    (chore) Bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/ansi-escape "1.0.1-5"
+(defproject com.github.strojure/ansi-escape "1.0.2-6-SNAPSHOT"
   :description "ANSI color escape sequences."
   :url "https://github.com/strojure/ansi-escape"
   :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
