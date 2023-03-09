55f227d15c90d417ceb3af399468023ea1624c0b
Sergey Trofimov <sergey@trofimov.name>
2023-03-09 15:39:49 +0100

    (chore): Release version `1.0.2-08`

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/ansi-escape "1.0.2-6-SNAPSHOT"
+(defproject com.github.strojure/ansi-escape "1.0.2-08"
   :description "ANSI color escape sequences."
   :url "https://github.com/strojure/ansi-escape"
   :license {:name "The Unlicense" :url "https://unlicense.org"}
